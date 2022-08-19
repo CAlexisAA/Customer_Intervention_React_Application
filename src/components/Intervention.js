bcf67ac25e6c1d42b30af9bfb877f6a631db3c26
Carlos Alexis <c.alexisalm.a@gmail.com>
2022-08-19 09:31:36 -0400

    2nd commit

---
+++ src/components/Intervention.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { Navigate } from "react-router-dom";
+
+function Intervention() {
+  return (
+    <div>
+        <h2>Create New Intervention</h2>
+        <input type='text' />
+    </div>
+  )
+}
+
+export default Intervention
