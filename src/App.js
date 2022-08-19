0b7cbe09508b2d1ad5e1ac2e23696aac097e63af
Felicia H <felicia.hartono@codeboxx.biz>
2022-08-19 07:14:28 -0700

    redirect on logout

--- src/App.js
+++ src/App.js
@@ -38,6 +38,7 @@ function App() {
     const Logout = () => {
         setUser({ name: "", email: "" });
         setShowForm(false);
+        navigate("/");
     };
     const navigate = useNavigate();
     return (
